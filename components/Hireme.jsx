98aa612717ec190452d11d76d85b02c3c2897ed5
vaqeh <34025faqih@gmail.com>
2023-06-16 16:31:04 +0700

    last

--- components/Hireme.jsx
+++ components/Hireme.jsx
@@ -17,7 +17,7 @@ const Hireme = () => {
             Do you want any work from me?
           </h2>
           <p className="lg:text-left text-justify max-w-lg text-sm mt-4 text-gray-200 leading-6">
-          A Junior Front-End Developer who is dedicated and passionate about creating engaging and responsive user experiences. Strong knowledge of HTML, CSS and JavaScript, as well as experience in teamwork and development projects. Can adapt quickly to new technologies and ready to continuously learn and develop in a dynamic environment.
+          Hire me for your next project! As a skilled front-end developer with expertise in JavaScript, ReactJS, Next.js, and Tailwind CSS, I am equipped to deliver outstanding results. I have a proven track record of creating responsive and interactive user interfaces that elevate user experiences. My knowledge of MongoDB further enhances my ability to handle data management tasks. With a strong work ethic and a passion for innovation, I am ready to tackle any challenge that comes my way. Don't miss the opportunity to collaborate with me on your project. Let's create something amazing together!
           </p>
           <button className="btn-primary mt-10">Say Hello</button>
         </div>
